c1e99a0ea88d435616259361a48b387bceda8594
indeedwatson <beethovenopus111@gmail.com>
2019-05-13 01:31:35 -0300

    calculate and print preferment

--- README.md
+++ README.md
@@ -11,5 +11,7 @@ Then follow the instructions
 
 TODO:
 - [X] autocomplete (basically stole it from stackexchange)
+- [X] calculate preferments
+- [ ] make separate function to calculate the total dough weight
+- [ ] make separate function to divide the dough
 - [ ] separate recipes into pizza, bread and others (such as tortillas)
-- [ ] make separate function to print the total dough weight
--- pyzza2.py
+++ pyzza2.py
@@ -34,28 +34,22 @@ def bakerCalc(flour, style=recipes['dopny']):
     Defaults to dopny if no recipe is specified
     """
     workingRecipe = {'flour': float(flour)}
-    if "poolish" in style.keys():
-        print("poolish".title())
-        pFlour = pWater = float(flour) * 0.09
-        pYeast = float(flour) * 0.0001
-        poolish = {"flour": pFlour, "water": pWater, "yeast": pYeast}
-        printMarkdownTable(poolish)
-        workingRecipe["flour"] = workingRecipe["flour"] - pFlour
-        for i in style.keys():
-            ingredient = float(workingRecipe["flour"]) * float(style[i]) / 100
-            workingRecipe[i] = ingredient
-    else:
-        for i in style.keys():
+    for i in style.keys():
+        if i == "starter":
+            print("starter")
+            printMarkdownTable(bakerCalc(flour, style["starter"]))
+        else:
             ingredient = float(flour) * float(style[i]) / 100
             workingRecipe[i] = ingredient
     return workingRecipe
 
 
-def printTotalWeight(recipe):
+def printTotal(totalWeight):
     # sum the total weight of the ingredients and divide by 260g
     # to get the amount of pies
     # totalWeight = lamda i: for i in recipe[i]
-    pies = round(totalWeight / 260)
+    pieWeight = input('> Weight of each pie: ').lower()
+    pies = round(totalWeight / (pieWeight if pieWeight else 260))
     print('That should be enough for ' + str(pies) + ' pies (' +
           str(round(totalWeight / pies)) + 'g each)\n')
 
@@ -70,6 +64,8 @@ def printMarkdownTable(ingredients):
         percent = float('%.2f'%(float(v))) * 100.0 / float(ingredients['flour'])
         table.append([k, v, percent])
         totalWeight = totalWeight + v
+    table.append(["---", "---", "---"])
+    table.append(["total", totalWeight, None])
     print(tabulate(table, headers, tablefmt="github", numalign="right"))
     print('\n')
 
@@ -101,11 +97,8 @@ while not choice.isdigit() or choice not in recipes:
         while not flour.isdigit():
             flour = input('Type the desired amount of flour: ')
         print("\n")
-        # print((choice.upper().center(19, '_')))
+        print(choice + "\n")
         printMarkdownTable(bakerCalc(flour, recipes[choice]))
     elif choice == 'exit' or choice in 'quit':
         print('Bye!')
     sys.exit()
-
-# TODO
-# implement biga, poolish and starter
